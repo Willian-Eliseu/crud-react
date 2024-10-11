4c5a806ed18dbd02f68f3dbe324c199ac923a302
Willian Eliseu da Silva <w.eliseu@gmail.com>
2024-10-11 10:22:18 -0300

    adicionados elementos de tabela e barra de navegação

--- crudfrontend/src/App.js
+++ crudfrontend/src/App.js
@@ -2,28 +2,41 @@ import Sobre from "./components/Sobre";
 import Alunos from "./components/Alunos";
 import Home from "./components/Home";
 import { BrowserRouter, Routes, Link, Route } from "react-router-dom";
-import {Nav} from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
-	return (
-		<div className="App">
-			<h1>Minha Aplicação React</h1>
-			<BrowserRouter>
-			
-			<Nav variant="tabs">
-				<Nav.Link as={Link} to="/">Página Inicial</Nav.Link>
-				<Nav.Link as={Link} to="/alunos">Cadastro de Alunos</Nav.Link>
-				<Nav.Link as={Link} to="/sobre">Sobre</Nav.Link>
-			</Nav>
-			
-			<Routes>
-				<Route path="/" element={<Home/>}></Route>
-				<Route path="/alunos" element={<Alunos/>}></Route>
-				<Route path="/sobre" element={<Sobre/>}></Route>
-			</Routes>
+  return (
+    <div className="App">
+      {/* <h1>Minha Aplicação React</h1> */}
+      <BrowserRouter>
+        <Navbar bg="light" data-bs-theme="light">
+          <Container>
+            <Navbar.Brand>
+              <Nav.Link as={Link} to="/">
+                Navbar
+              </Nav.Link>
+            </Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">
+                Página Inicial
+              </Nav.Link>
+              <Nav.Link as={Link} to="/alunos">
+                Cadastro de Alunos
+              </Nav.Link>
+              <Nav.Link as={Link} to="/sobre">
+                Sobre
+              </Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
 
-			</BrowserRouter>
-		</div>		
-	);
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/alunos" element={<Alunos />}></Route>
+          <Route path="/sobre" element={<Sobre />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
--- crudfrontend/src/components/Alunos.js
+++ crudfrontend/src/components/Alunos.js
@@ -1,5 +1,71 @@
-export default function Alunos(){
+import React from "react";
+import { Table } from "react-bootstrap";
+
+class Alunos extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      alunos: [],
+    };
+  }
+
+  render() {
     return (
-        <h4>Esta é a página Alunos</h4>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Password</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Nome</td>
+            <td>Sobrenome</td>
+            <td>emailteste@email.com</td>
+            <td>asenhadousuario</td>
+            <td>+55-9-14-1234-1234</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Nome</td>
+            <td>Sobrenome</td>
+            <td>emailteste@email.com</td>
+            <td>asenhadousuario</td>
+            <td>+55-9-14-1234-1234</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Nome</td>
+            <td>Sobrenome</td>
+            <td>emailteste@email.com</td>
+            <td>asenhadousuario</td>
+            <td>+55-9-14-1234-1234</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Nome</td>
+            <td>Sobrenome</td>
+            <td>emailteste@email.com</td>
+            <td>asenhadousuario</td>
+            <td>+55-9-14-1234-1234</td>
+          </tr>
+        </tbody>
+      </Table>
     );
+  }
 }
+
+// export default function Alunos(){
+//     return (
+//         <h4>Esta é a página Alunos</h4>
+//     );
+// }
+
+export default Alunos;
